58fc7be3d71354396a5e9288c6e533cf0e510c7e
Ihor <ihorfront88@gmail.com>
2023-03-01 18:57:48 +0200

    refactor: files for start project, installed gh-pages for build project

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    </>
+  );
+};
+
+export default App;
