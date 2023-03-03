0ea937bd07b04127a900769e87b7ce2577c9a457
Ihor <ihorfront88@gmail.com>
2023-03-04 00:06:40 +0200

    responsive to about, projects sections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,8 @@ const App = () => {
   return (
     <div className="container">
       <Hero />
-      {/* <About /> */}
-      {/* <Projects /> */}
+      <About />
+      <Projects />
     </div>
   );
 };
